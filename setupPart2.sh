9319059e618a57096caf7bc38c540c66b1d0d0f6
YOUR NAME <behnkej@me.com>
2016-05-27 19:16:30 -0400

    Changed naming structure and added XCode Package Control

--- setupPart2.sh
+++ setupPart2.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#Setting up Sublime Text
 #Ensuring Sublime Text has been opened at least once so support files are generated
 echo "Opening Sublime Text"
 open -a Sublime\ Text
@@ -14,8 +13,6 @@ echo "Downloading Package Control"
 curl -o /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package https://packagecontrol.io/Package%20Control.sublime-package
 echo "Done!"
 
-#Set up Alcatraz for XCode
-curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
 
 
 
