8d22b0d579e7c030edae6f4839885fe7dbd4defe
John Behnke <behnkej@me.com>
2018-01-28 23:00:13 -0500

    Seems to be working?
    Need to verify… maybe do a clean install on the laptop…

--- setup.sh
+++ setup.sh
@@ -11,27 +11,27 @@ RESET=$(tput sgr0)
 
 read -p "Press ${BLUE}any${RESET} key to start the install process: "
 
-echo "${BLUE}Staring install process...${RESET}"
+echo "\n${BLUE}Staring install process...${RESET}"
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # System Changes
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo "${RED}Disabling${RESET} GateKeeper"
+echo "\n${RED}Disabling${RESET} GateKeeper"
 sudo spctl --master-disable
 sudo defaults write /Library/Preferences/com.apple.security GKAutoRearm -bool NO 
 
-echo "${RED}Disabling${RESET} dashboard"
+echo "\n${RED}Disabling${RESET} dashboard"
 defaults write com.apple.dashboard mcx-disabled -boolean YES; killall Dock 
 
-echo "${GREEN}Adding${RESET} blank space to dock"
+echo "\n${GREEN}Adding${RESET} blank space to dock"
 defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
 
-echo "${BLUE}Setting${RESET} key repeat and key delay to be faster (${RED}reboot required${RESET})"
+echo "\n${BLUE}Setting${RESET} key repeat and key delay to be faster (${RED}reboot required${RESET})"
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
-echo "${BLUE}Configuring${RESET} dock to autohide"
+echo "\n${BLUE}Configuring${RESET} dock to autohide"
 osascript <<EOD
   tell application "System Events"
     if (get autohide of dock preferences) is false then
@@ -44,15 +44,15 @@ EOD
 # Development Tools
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-echo "${GREEN}Installing${RESET} Xcode Command Line tools"
+echo "\n${GREEN}Installing${RESET} Xcode Command Line tools"
 xcode-select --install
 
 read -p "Press ${BLUE}any${RESET} key once the Xcode Command Line tools are done installing..."
 
-echo "${GREEN}Installing${RESET} oh-my-zsh"
+echo "\n${GREEN}Installing${RESET} oh-my-zsh"
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-echo "${GREEN}Installing${RESET} Homebrew"
+echo "\n${GREEN}Installing${RESET} Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
@@ -68,7 +68,7 @@ brew_packages=(
 	"python3"
 )
 
-echo "${GREEN}Installing${RESET} Homebrew packages"
+echo "\n${GREEN}Installing${RESET} Homebrew packages"
 
 for package in "${brew_packages[@]}"; do
 	brew install "$package"
@@ -87,7 +87,7 @@ brew_cask_applications=(
 	"docker"
 )
 
-echo "${GREEN}Installing${RESET} Homebrew Cask Applications"
+echo "\n${GREEN}Installing${RESET} Homebrew Cask Applications"
 
 for cask_package in "${brew_cask_applications[@]}"; do
   brew cask install "$cask_package"
@@ -113,13 +113,15 @@ done
 
 read -p "Press ${BLUE}any${RESET} key once you've setup iCloud Documents..."
 
+
+
 open Personal.terminal
 defaults write com.apple.Terminal "Default Window Settings" -string 'Personal'
 defaults write com.apple.Terminal "Startup Window Settings" -string 'Personal'
 
+cd ~
 
-killall Dock
-
+cd /Users/john/Documents/Programming/Personal/dotfiles/terminal
 
 rm ~/.zshrc
 
@@ -130,9 +132,6 @@ rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -s ~/Documents/Programming/Personal/dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
 
-
-
-echo "🍻🍻🍻🍻🍻Finished! Enjoy! 🍻🍻🍻🍻🍻"
 
 	
 
